b1f8983db62df8d5bee3f3479a499ea012023053
JOKOJOKO405 <biz.kj405@gmail.com>
2021-06-22 21:13:47 +0900

    add コンポーネント作成

---
+++ components/AppBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn @click.prevent="clickAction" color="primary" class="font-weight-bold text-body-1" elevation="0" large>
+    {{ btnText }}
+  </v-btn>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'AppBtn',
+  props: {
+    btnText: {
+      type: String,
+      required: true
+    },
+    clickAction: {
+      type: () => {},
+      required: true
+    }
+  }
+})
+</script>
---
+++ components/AppDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog :value="isOpenedDialog">
+    <v-card class="pa-3">
+      <v-row no-gutters class="align-center">
+        <v-card-title class="pa-3">
+          {{ dialogTitle }}
+        </v-card-title>
+        <v-spacer />
+        <v-btn @click="clickAction" icon><v-icon>mdi-close</v-icon></v-btn>
+      </v-row>
+      <v-divider class="mb-6" />
+      <v-card-text>
+        <slot />
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  name: 'AppDialog',
+  props: {
+    isOpenedDialog: {
+      type: Boolean,
+      default: false
+    },
+    dialogTitle: {
+      type: String,
+      required: true
+    },
+    clickAction: {
+      type: () => {},
+      required: true
+    }
+  },
+  setup(props) {}
+})
+</script>
