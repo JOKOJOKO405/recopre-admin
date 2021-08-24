95acfbc665cb062d6ec0890daaad9d859aba2b4a
JOKOJOKO405 <biz.kj405@gmail.com>
2021-08-24 20:36:24 +0900

    add todoページ

---
+++ pages/_id/_childId/todo.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container class="pa-12 my-auto">
+    <h1 class="text-center mb-10">
+      <i class="mdi mdi-clock-time-four-outline primary--text" />
+      あと{{ time }}
+    </h1>
+    <v-form class="todo-form-bottom">
+      <div
+        v-for="(list, index) in lists"
+        :key="index"
+        class="todo-form py-6 d-flex justify-space-between align-center"
+      >
+        <p class="ma-0">{{ list.text }}</p>
+        <p class="ma-0">
+          <v-switch inset v-model="list.value" />
+        </p>
+      </div>
+      <AppBtn btn-text="おわった！" class="d-block mx-auto mt-10" />
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    const lists = reactive([
+      { text: 'きがえ', value: false },
+      { text: 'はみがき', value: false },
+      { text: '給食セット', value: false }
+    ])
+    const time = '10分32秒'
+    return { lists, time }
+  }
+})
+</script>
+
+<style scoped>
+.todo-form {
+  border-bottom: 1px solid #ccc;
+}
+.todo-form-bottom {
+  border-top: 1px solid #ccc;
+}
+</style>
