0a55b84b40049af5bea97d18b41760e5421cbf9c
JOKOJOKO405 <biz.kj405@gmail.com>
2021-08-25 09:08:04 +0900

    add シールページ

---
+++ pages/_id/_childId/seal.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row></v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
