f8b4e4d39f818ebb2b6dc4883180203e335ef048
JOKOJOKO405 <biz.kj405@gmail.com>
2021-09-20 15:56:36 +0900

    add appbar

---
+++ components/AppBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app-bar flat app color="white" height="64">
+    <v-avatar color="primary">
+      <v-icon dark> mdi-account-circle </v-icon>
+    </v-avatar>
+  </v-app-bar>
+</template>
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppBar',
+  setup() {}
+})
+</script>
